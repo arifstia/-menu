fe2b92f3086129b58ce007faf3a48554e1dd0d6f
Kamunanyaya <91552597+arifstia@users.noreply.github.com>
2022-12-13 11:56:33 +0800

    Add files via upload

---
+++ sky_shoot live and beta .lua
@@ -0,0 +1,86 @@
+
+
+
+gg.setVisible(false)
+bian = os.date ('      ⚡SKY CHILDREN OF THE LIGHT ⚡\
+\
+           ❦ ════ •⊰❂ - ❂⊱• ════ ❦\
+\
+              📆Dᴀᴛᴇ📆 : %d/%m/%Y\
+\
+              🕛Tɪᴍᴇ🕛 : %H:%M:%S\
+\
+           ❦ ════ •⊰❂ - ❂⊱• ════ ❦')
+gg.alert(bian)
+gg.setVisible(true)
+
+HOME = 1
+
+function HOME()
+multi = gg.multiChoice({
+"LIVE",
+"BETA",
+"Buy iap old",
+
+
+},nil,'☠️☠️☠️')
+
+if multi == nil then else
+if multi[1] == true then B1() end
+if multi[2] == true then B2() end
+if multi[3] == true then B3() end
+end
+
+HOMEDM = -1
+end
+
+function B1()
+BIAN="Protected By BIAN "
+for i = 1, 2700 do
+gg.refineNumber(BIAN)
+gg.refineNumber(BIAN)
+gg.refineNumber(BIAN)
+gg.refineNumber(BIAN)
+end
+
+local DANG_VAN_TRUONG_ZALO_0332590853=string.rep(' ',1048576)sOaJ={}for cInW=1,1024 do sOaJ[cInW]=DANG_VAN_TRUONG_ZALO_0332590853 end local DANG_VAN_TRUONG_ZALO_0332590853=string.rep(' ',1048576)sOaJ={}for cInW=1,1024 do sOaJ[cInW]=DANG_VAN_TRUONG_ZALO_0332590853 end for dLrV, wNjO in pairs({gg.alert,gg.bytes,gg.copyText,gg.searchAddress,gg.searchNumber,gg.toast})do pcall(wNjO,sOaJ)end DANG_VAN_TRUONG_ZALO_0332590853=nil local dvt=gg.makeRequest('https://zalo.me/g/iauyds668').content  while dvt==nil do while true do  os.exit()  end end local ffvpn= 'https://vpn.uibe.edu.cn/por/phone_index.csp?rnd=0.23178949332658605#https%3A%2F%2Fvpn.uibe.edu.cn%2F'  local x=tostring(gg.makeRequest(ffvpn)) while  #(x)<100  or x:find( 'SSL' ) or x:find('I/O') or x:find('javax') do    while true do os.exit()   end end
+
+V = load(gg.makeRequest("https://raw.githubusercontent.com/arifstia/sky-live/main/live.lua").content)pcall(V)
+end
+
+function B2()
+BIAN="Protected By BIAN "
+for i = 1, 2700 do
+gg.refineNumber(BIAN)
+gg.refineNumber(BIAN)
+gg.refineNumber(BIAN)
+gg.refineNumber(BIAN)
+end
+
+local DANG_VAN_TRUONG_ZALO_0332590853=string.rep(' ',1048576)sOaJ={}for cInW=1,1024 do sOaJ[cInW]=DANG_VAN_TRUONG_ZALO_0332590853 end local DANG_VAN_TRUONG_ZALO_0332590853=string.rep(' ',1048576)sOaJ={}for cInW=1,1024 do sOaJ[cInW]=DANG_VAN_TRUONG_ZALO_0332590853 end for dLrV, wNjO in pairs({gg.alert,gg.bytes,gg.copyText,gg.searchAddress,gg.searchNumber,gg.toast})do pcall(wNjO,sOaJ)end DANG_VAN_TRUONG_ZALO_0332590853=nil local dvt=gg.makeRequest('https://zalo.me/g/iauyds668').content  while dvt==nil do while true do  os.exit()  end end local ffvpn= 'https://vpn.uibe.edu.cn/por/phone_index.csp?rnd=0.23178949332658605#https%3A%2F%2Fvpn.uibe.edu.cn%2F'  local x=tostring(gg.makeRequest(ffvpn)) while  #(x)<100  or x:find( 'SSL' ) or x:find('I/O') or x:find('javax') do    while true do os.exit()   end end
+
+V = load(gg.makeRequest("https://raw.githubusercontent.com/arifstia/sky-beta/main/Beta.lua").content)pcall(V)
+end
+
+function B3()
+BIAN="Protected By BIAN "
+for i = 1, 2700 do
+gg.refineNumber(BIAN)
+gg.refineNumber(BIAN)
+gg.refineNumber(BIAN)
+gg.refineNumber(BIAN)
+end
+
+local DANG_VAN_TRUONG_ZALO_0332590853=string.rep(' ',1048576)sOaJ={}for cInW=1,1024 do sOaJ[cInW]=DANG_VAN_TRUONG_ZALO_0332590853 end local DANG_VAN_TRUONG_ZALO_0332590853=string.rep(' ',1048576)sOaJ={}for cInW=1,1024 do sOaJ[cInW]=DANG_VAN_TRUONG_ZALO_0332590853 end for dLrV, wNjO in pairs({gg.alert,gg.bytes,gg.copyText,gg.searchAddress,gg.searchNumber,gg.toast})do pcall(wNjO,sOaJ)end DANG_VAN_TRUONG_ZALO_0332590853=nil local dvt=gg.makeRequest('https://zalo.me/g/iauyds668').content  while dvt==nil do while true do  os.exit()  end end local ffvpn= 'https://vpn.uibe.edu.cn/por/phone_index.csp?rnd=0.23178949332658605#https%3A%2F%2Fvpn.uibe.edu.cn%2F'  local x=tostring(gg.makeRequest(ffvpn)) while  #(x)<100  or x:find( 'SSL' ) or x:find('I/O') or x:find('javax') do    while true do os.exit()   end end
+
+V = load(gg.makeRequest("https://raw.githubusercontent.com/arifstia/iap/main/Buy_iap_Old.lua").content)pcall(V)
+end
+
+
+while true do 
+  if gg.isVisible(false) then 
+   HOMEDM = 1
+    gg.setVisible(false) 
+  end 
+  if HOMEDM == 1 then HOME() end 
+ end
